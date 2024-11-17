0946b973f60f44289a5d2c46c9deeb03ce4dd01d
OmgRod <rlimafonseca2010@gmail.com>
2024-11-17 09:15:17 +0000

    debugging and stuff

--- loader/src/translation/Translator.cpp
+++ loader/src/translation/Translator.cpp
@@ -12,12 +12,15 @@ void Translator::setLanguage(int64_t languageSetting) {
     switch (languageSetting) {
         case 0:
             currentLang = "en-us";
+            log::debug("Language set to English (en-us)");
             break;
         case 1:
             currentLang = "pt-pt";
+            log::debug("Language set to Portuguese (pt-pt)");
             break;
         default:
             currentLang = "en-us";
+            log::debug("Language set to default English (en-us)");
             break;
     }
 
@@ -41,6 +44,12 @@ void Translator::loadTranslations() {
             {"geode.mods.modtoberTab", "Modtober"}
         }}
     };
+
+    // Log translations to verify
+    log::debug("Translations loaded for language: {}", currentLang);
+    for (const auto& [key, value] : translations[currentLang]) {
+        log::debug("Loaded translation: {} -> {}", key, value);
+    }
 }
 
 std::string Translator::getTranslation(const std::string& translationKey) const {
